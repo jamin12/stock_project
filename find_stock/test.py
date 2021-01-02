f7440a1940e2fd5914efcdc53e76b49dbdb10f0d
jamin12 <rudals9901@naver.com>
2021-01-02 16:57:19 +0900

    .

--- find_stock/test.py
+++ find_stock/test.py
@@ -22,13 +22,14 @@ def into_selenium(url):
     broswer.get(url)
     return broswer
 
+
 if __name__ == '__main__':
     #엑셀파일에서 종목 코드 찾기
     wb = load_workbook("./find_stock/stock_codenum.xlsm",data_only = True)
     ws = wb.active
     stock_code = ""
-    for stocknum in ws.iter_rows(min_row=1):
-        if "유니퀘스트" == stocknum[0].value:
+    for stocknum in ws.iter_rows(min_row=2):
+        if "ESR켄달스퀘어리츠" == stocknum[0].value:
             stock_code = str(stocknum[4].value)
             break
     
@@ -83,7 +84,7 @@ if __name__ == '__main__':
     EV_EBITDA = []
     PSR = []
     PBR = []
-    industries_avg = (broswer.find_element_by_xpath('/html/body/div/form/div[1]/div/div[2]/div[1]/div/table/tbody/tr[3]/td/dl/dt[4]/b').text).replace(',','')
+    INDUSTRIES_AVG_PER = (broswer.find_element_by_xpath('/html/body/div/form/div[1]/div/div[2]/div[1]/div/table/tbody/tr[3]/td/dl/dt[4]/b').text).replace(',','')
     for i in range(2,7):
         EPS.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[9]/table[2]/tbody/tr[1]/td[{i}]').text).replace(',',''))
         PER.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[9]/table[2]/tbody/tr[17]/td[{i}]').text).replace(',',''))
@@ -118,34 +119,88 @@ if __name__ == '__main__':
         net_debt_ratio.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[5]/table[2]/tbody/tr[10]/td[{i}]').text).replace(',',''))
         current_rate.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[5]/table[2]/tbody/tr[13]/td[{i}]').text).replace(',',''))
         current_account_ratio.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[5]/table[2]/tbody/tr[16]/td[{i}]').text).replace(',',''))
-    
+    #성장성
+    #매출액 증가율, 영업 이익 증가율, 순이익 증가율
+    broswer.find_element_by_id("val_td2").click()
+    time.sleep(0.5)
+    sales_growth_rate = []
+    operating_profit_growth_rate = []
+    net_profit_growth_rate = []
+    for i in range(2,7):
+        sales_growth_rate.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[5]/table[2]/tbody/tr[1]/td[{i}]').text).replace(',',''))
+        operating_profit_growth_rate.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[5]/table[2]/tbody/tr[4]/td[{i}]').text).replace(',',''))
+        net_profit_growth_rate.append((broswer.find_element_by_xpath(f'/html/body/div/form/div[1]/div/div[2]/div[3]/div/div/div[5]/table[2]/tbody/tr[7]/td[{i}]').text).replace(',',''))
     
 
     #평가기준
     score = 0
     score_criteria2 = 0 # 몇년간 상승했는지 체크
-    for i in range(4):
-        #순 부채 비율이 30%이하 인지
-        if float(net_debt_ratio[i+1]) < 30:
-            score_criteria2 = score_criteria2 + 1
-        if score_criteria2 >= 3:
-            score = score + 3
-            score_criteria2 = 0
-        #투자활동 활동과 재무활동이 모두 + 인지
-        if float(investment_activities[i+1]) > 0 and float(financial_activities[i+1]) > 0:
-            score_criteria2 = score_criteria2 + 1
-        if score_criteria2 >= 3:
-            score = score - 3
-            score_criteria2 = 0
-        # EPS와 ROE가 꾸준히 오르는가
-        if float(EPS[i]) < float(EPS[i+1]) or float(ROE[i]) < float(ROE[i+1]):
-            score_criteria2 = score_criteria2 + 1
-        if score_criteria2 >= 3:
-            score = score + 3
-            score_criteria2 = 0
-    print(score)
+    #순 부채 비율이 30%이하 인지
+    try:
+        for i in range(4):
+            if float(net_debt_ratio[i+1]) < 30:
+                score_criteria2 = score_criteria2 + 1
+            if score_criteria2 >= 3:
+                score = score + 3
+                score_criteria2 = 0
+    except:
+        pass
 
+    #투자활동 활동과 재무활동이 모두 + 인지
+    try:
+        for i in range(4):
+            if float(investment_activities[i+1]) > 0 and float(financial_activities[i+1]) > 0:
+                score_criteria2 = score_criteria2 + 1
+            if score_criteria2 >= 3:
+                score = score - 3
+                score_criteria2 = 0
+    except:
+        pass
 
+    # EPS와 ROE가 꾸준히 오르는가
+    try:
+        for i in range(4):
+            if float(EPS[i]) < float(EPS[i+1]) or float(ROE[i]) < float(ROE[i+1]):
+                score_criteria2 = score_criteria2 + 1
+            if score_criteria2 >= 3:
+                score = score + 3
+                score_criteria2 = 0
+    except:
+        pass
+
+    # 유동비율 ,당좌비율이 100%가 넘고 부채비율이 200%인지
+    try:
+        for i in range(4):
+            if (float(current_liability_ratio[i+1]) > 100 and float(current_account_ratio[i+1]) > 100) and float(debt_ratio[i+1]) < 200:
+                score_criteria2 = score_criteria2 + 1
+            if score_criteria2 >= 3:
+                score = score + 3
+                score_criteria2 = 0
+    except:
+        pass
+    
+    # 영업이익률이, 매출이익률,당기이익률 계속 오르는지
+    try:
+        for i in range(4):
+            if (float(sales_growth_rate[i+1]) > 0 and float(operating_profit_growth_rate[i+1]) > 0) or float(net_profit_growth_rate[i+1]) > 0:
+                score_criteria2 = score_criteria2 + 1
+            if score_criteria2 >= 3:
+                score = score + 3
+                score_criteria2 = 0
+    except:
+        pass
+    # 동종업계 평균보다 PER이 낮은가
+    try:
+        for i in range(4):
+            if float(PER[i+1]) < float(INDUSTRIES_AVG_PER):
+                score_criteria2 = score_criteria2 + 1
+            if score_criteria2 >= 3:
+                score = score + 3
+                score_criteria2 = 0
+    except:
+        pass
+
+    print((score/12)*100)
 
 
 
